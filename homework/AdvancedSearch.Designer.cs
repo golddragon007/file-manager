0fedecf924c5eaebb679db96a885c72464a8ef39
zsuzsi <12kzsk@gmail.com>
2015-11-18 22:51:04 +0100

    AdvancedSearch.cs added

---
+++ homework/AdvancedSearch.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace homework
+{
+    partial class AdvancedSearch
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxTags = new System.Windows.Forms.TextBox();
+            this.labelTags = new System.Windows.Forms.Label();
+            this.labelAdded = new System.Windows.Forms.Label();
+            this.textBoxDOI = new System.Windows.Forms.TextBox();
+            this.textBoxAuthor = new System.Windows.Forms.TextBox();
+            this.textBoxTitle = new System.Windows.Forms.TextBox();
+            this.labelDOI = new System.Windows.Forms.Label();
+            this.labelYear = new System.Windows.Forms.Label();
+            this.labelAuthor = new System.Windows.Forms.Label();
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxAddedFrom = new System.Windows.Forms.TextBox();
+            this.textBoxAddedTo = new System.Windows.Forms.TextBox();
+            this.labelFavorite = new System.Windows.Forms.Label();
+            this.checkBoxFavourite = new System.Windows.Forms.CheckBox();
+            this.textBoxYearTo = new System.Windows.Forms.TextBox();
+            this.textBoxYearFrom = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.textBoxNotes = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxTags
+            // 
+            this.textBoxTags.Location = new System.Drawing.Point(91, 137);
+            this.textBoxTags.Name = "textBoxTags";
+            this.textBoxTags.Size = new System.Drawing.Size(207, 20);
+            this.textBoxTags.TabIndex = 38;
+            // 
+            // labelTags
+            // 
+            this.labelTags.AutoSize = true;
+            this.labelTags.Location = new System.Drawing.Point(30, 140);
+            this.labelTags.Name = "labelTags";
+            this.labelTags.Size = new System.Drawing.Size(34, 13);
+            this.labelTags.TabIndex = 37;
+            this.labelTags.Text = "Tags:";
+            // 
+            // labelAdded
+            // 
+            this.labelAdded.AutoSize = true;
+            this.labelAdded.Location = new System.Drawing.Point(30, 169);
+            this.labelAdded.Name = "labelAdded";
+            this.labelAdded.Size = new System.Drawing.Size(41, 13);
+            this.labelAdded.TabIndex = 33;
+            this.labelAdded.Text = "Added:";
+            // 
+            // textBoxDOI
+            // 
+            this.textBoxDOI.Location = new System.Drawing.Point(91, 108);
+            this.textBoxDOI.Name = "textBoxDOI";
+            this.textBoxDOI.Size = new System.Drawing.Size(207, 20);
+            this.textBoxDOI.TabIndex = 32;
+            // 
+            // textBoxAuthor
+            // 
+            this.textBoxAuthor.Location = new System.Drawing.Point(91, 50);
+            this.textBoxAuthor.Name = "textBoxAuthor";
+            this.textBoxAuthor.Size = new System.Drawing.Size(207, 20);
+            this.textBoxAuthor.TabIndex = 30;
+            // 
+            // textBoxTitle
+            // 
+            this.textBoxTitle.Location = new System.Drawing.Point(91, 21);
+            this.textBoxTitle.Name = "textBoxTitle";
+            this.textBoxTitle.Size = new System.Drawing.Size(207, 20);
+            this.textBoxTitle.TabIndex = 29;
+            // 
+            // labelDOI
+            // 
+            this.labelDOI.AutoSize = true;
+            this.labelDOI.Location = new System.Drawing.Point(30, 111);
+            this.labelDOI.Name = "labelDOI";
+            this.labelDOI.Size = new System.Drawing.Size(29, 13);
+            this.labelDOI.TabIndex = 28;
+            this.labelDOI.Text = "DOI:";
+            // 
+            // labelYear
+            // 
+            this.labelYear.AutoSize = true;
+            this.labelYear.Location = new System.Drawing.Point(30, 82);
+            this.labelYear.Name = "labelYear";
+            this.labelYear.Size = new System.Drawing.Size(35, 13);
+            this.labelYear.TabIndex = 27;
+            this.labelYear.Text = "Year: ";
+            // 
+            // labelAuthor
+            // 
+            this.labelAuthor.AutoSize = true;
+            this.labelAuthor.Location = new System.Drawing.Point(30, 53);
+            this.labelAuthor.Name = "labelAuthor";
+            this.labelAuthor.Size = new System.Drawing.Size(55, 13);
+            this.labelAuthor.TabIndex = 26;
+            this.labelAuthor.Text = "Author(s): ";
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Location = new System.Drawing.Point(30, 24);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(33, 13);
+            this.labelTitle.TabIndex = 25;
+            this.labelTitle.Text = "Title: ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(88, 170);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 39;
+            this.label1.Text = "From:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(199, 170);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 13);
+            this.label2.TabIndex = 40;
+            this.label2.Text = "To:";
+            // 
+            // textBoxAddedFrom
+            // 
+            this.textBoxAddedFrom.Location = new System.Drawing.Point(127, 166);
+            this.textBoxAddedFrom.Name = "textBoxAddedFrom";
+            this.textBoxAddedFrom.Size = new System.Drawing.Size(66, 20);
+            this.textBoxAddedFrom.TabIndex = 41;
+            // 
+            // textBoxAddedTo
+            // 
+            this.textBoxAddedTo.Location = new System.Drawing.Point(220, 166);
+            this.textBoxAddedTo.Name = "textBoxAddedTo";
+            this.textBoxAddedTo.Size = new System.Drawing.Size(78, 20);
+            this.textBoxAddedTo.TabIndex = 42;
+            // 
+            // labelFavorite
+            // 
+            this.labelFavorite.AutoSize = true;
+            this.labelFavorite.Location = new System.Drawing.Point(30, 223);
+            this.labelFavorite.Name = "labelFavorite";
+            this.labelFavorite.Size = new System.Drawing.Size(48, 13);
+            this.labelFavorite.TabIndex = 36;
+            this.labelFavorite.Text = "Favorite:";
+            // 
+            // checkBoxFavourite
+            // 
+            this.checkBoxFavourite.AutoSize = true;
+            this.checkBoxFavourite.Location = new System.Drawing.Point(91, 228);
+            this.checkBoxFavourite.Name = "checkBoxFavourite";
+            this.checkBoxFavourite.Size = new System.Drawing.Size(15, 14);
+            this.checkBoxFavourite.TabIndex = 47;
+            this.checkBoxFavourite.UseVisualStyleBackColor = true;
+            // 
+            // textBoxYearTo
+            // 
+            this.textBoxYearTo.Location = new System.Drawing.Point(220, 79);
+            this.textBoxYearTo.Name = "textBoxYearTo";
+            this.textBoxYearTo.Size = new System.Drawing.Size(78, 20);
+            this.textBoxYearTo.TabIndex = 51;
+            // 
+            // textBoxYearFrom
+            // 
+            this.textBoxYearFrom.Location = new System.Drawing.Point(127, 79);
+            this.textBoxYearFrom.Name = "textBoxYearFrom";
+            this.textBoxYearFrom.Size = new System.Drawing.Size(66, 20);
+            this.textBoxYearFrom.TabIndex = 50;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(199, 82);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(23, 13);
+            this.label5.TabIndex = 49;
+            this.label5.Text = "To:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(88, 82);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(33, 13);
+            this.label6.TabIndex = 48;
+            this.label6.Text = "From:";
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(127, 254);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(75, 23);
+            this.buttonSearch.TabIndex = 52;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // textBoxNotes
+            // 
+            this.textBoxNotes.Location = new System.Drawing.Point(92, 197);
+            this.textBoxNotes.Name = "textBoxNotes";
+            this.textBoxNotes.Size = new System.Drawing.Size(207, 20);
+            this.textBoxNotes.TabIndex = 54;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 196);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 53;
+            this.label3.Text = "Notes:";
+            // 
+            // AdvancedSearch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(328, 294);
+            this.Controls.Add(this.textBoxNotes);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.textBoxYearTo);
+            this.Controls.Add(this.textBoxYearFrom);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.checkBoxFavourite);
+            this.Controls.Add(this.textBoxAddedTo);
+            this.Controls.Add(this.textBoxAddedFrom);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxTags);
+            this.Controls.Add(this.labelTags);
+            this.Controls.Add(this.labelFavorite);
+            this.Controls.Add(this.labelAdded);
+            this.Controls.Add(this.textBoxDOI);
+            this.Controls.Add(this.textBoxAuthor);
+            this.Controls.Add(this.textBoxTitle);
+            this.Controls.Add(this.labelDOI);
+            this.Controls.Add(this.labelYear);
+            this.Controls.Add(this.labelAuthor);
+            this.Controls.Add(this.labelTitle);
+            this.Name = "AdvancedSearch";
+            this.Text = "AdvancedSearch";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxTags;
+        private System.Windows.Forms.Label labelTags;
+        private System.Windows.Forms.Label labelAdded;
+        private System.Windows.Forms.TextBox textBoxDOI;
+        private System.Windows.Forms.TextBox textBoxAuthor;
+        private System.Windows.Forms.TextBox textBoxTitle;
+        private System.Windows.Forms.Label labelDOI;
+        private System.Windows.Forms.Label labelYear;
+        private System.Windows.Forms.Label labelAuthor;
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxAddedFrom;
+        private System.Windows.Forms.TextBox textBoxAddedTo;
+        private System.Windows.Forms.Label labelFavorite;
+        private System.Windows.Forms.CheckBox checkBoxFavourite;
+        private System.Windows.Forms.TextBox textBoxYearTo;
+        private System.Windows.Forms.TextBox textBoxYearFrom;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.TextBox textBoxNotes;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ homework/AdvancedSearch.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace homework
+{
+    public partial class AdvancedSearch : Form
+    {
+
+        private ASCriteria criteria;
+
+        public ASCriteria Criteria
+        {
+            get { return criteria; }
+        }
+
+        public AdvancedSearch()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            textBoxYearFrom.ForeColor = Color.White;
+
+            int yearFrom = 0;
+            int yearTo = 0;
+            DateTime addedFrom = DateTime.MinValue;
+            DateTime addedTo = DateTime.MinValue;
+            string errorMessage = "";
+
+            if ((textBoxYearFrom.Text != "") && !(Int32.TryParse(textBoxYearFrom.Text, out yearFrom)))
+            {
+                errorMessage += "Not valid year format. (Year: From:) Please give an integer.\n";
+                textBoxYearFrom.ForeColor = Color.Red;
+            }
+
+            if ((textBoxYearTo.Text != "") && !(Int32.TryParse(textBoxYearTo.Text, out yearTo)))
+            {
+                errorMessage += "Not valid year format. (Year: To:) Please give an integer.\n";
+                textBoxYearTo.ForeColor = Color.Red;
+            }
+
+            if ((textBoxAddedFrom.Text != "") && !(DateTime.TryParse(textBoxAddedFrom.Text, out addedFrom)))
+            {
+                errorMessage += "Not valid date format. (Added: From:) Please give a date.\n";
+                textBoxAddedFrom.ForeColor = Color.Red;
+            }
+
+            if ((textBoxAddedTo.Text != "") && !(DateTime.TryParse(textBoxAddedTo.Text, out addedTo)))
+            {
+                errorMessage += "Not valid date format. (Added: To:) Please give a date.\n";
+                textBoxAddedTo.ForeColor = Color.Red;
+            }
+
+            if (errorMessage == "")
+            {
+                criteria = new ASCriteria(textBoxTitle.Text, textBoxAuthor.Text, yearFrom, yearTo, textBoxDOI.Text, textBoxTags.Text,
+                    addedFrom, addedTo, textBoxNotes.Text, checkBoxFavourite.Checked);
+                this.DialogResult = System.Windows.Forms.DialogResult.OK;
+            }
+            else
+            {
+                MessageBox.Show(errorMessage);
+            }
+        }
+    }
+}
